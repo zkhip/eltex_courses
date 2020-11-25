8a775a52d55e820cf3d34b124e1c2d1d6d344e48
zkhip <v.derevtsova@cyber.sibsutis.ru>
2020-11-25 15:09:45 +0700

    Add pointers first task

---
+++ pointers/task_1.c
@@ -0,0 +1,27 @@
+/*  1. print all bytes
+    2. change third byte  */
+
+#include <stdio.h>
+
+int main() {
+    unsigned int x = 0xA1B2C3D4;
+    printf("Original number: %X\n\n", x);
+
+    unsigned char* ptr = &x;
+    for (int i = 0; i < 4; i++) {
+        printf("%d byte: %X\n", i + 1, *(ptr + i));
+    } 
+
+    unsigned int new_byte;
+    printf("\nEnter new 3 byte: ");
+    scanf("%X", &new_byte);
+    if (new_byte < 0 || new_byte > 0xFF) {
+        printf("\nError! Allowable interval [0; 0xFF].\n");
+        return 1;
+    }
+    ptr += 2;
+    *ptr = new_byte;
+    printf("\nNew number: %X\n", x);
+
+    return 0;
+}
